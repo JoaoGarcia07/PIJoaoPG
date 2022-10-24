a1affdb1d67e209d6043c6dbe9ea4b0ad0d6deaa
JoaoGarcia07 <joao.garcia2@alunos.sc.senac.br>
2022-10-24 17:12:13 -0300

    add button p/ ir a outra pag

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View } from 'react-native';
-export function HomeScreen(){
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export function HomeScreen({navigation}){
+  const irParaSobre = () => {
+    return navigation.nagate("Sobre");
+  };
+
+
   return(
     <View style={styles.container}>
       <Text>OLá felas</Text>
+      <Button title="Ir para sobre"
+       onPress={()=>navigation.navigate("Sobre")}
+       />
     </View>
   )
 }
+
+function SobreScreen(){
+  return(
+    <View>
+      <Text>Olá eu sou a pagina sobre</Text>
+    </View>
+  )
+}
+
 const Stack =createNativeStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
       <Stack.Screen name={"Home"} component={HomeScreen}/>
+      <Stack.Screen name={"Sobre"} component={SobreScreen}/>
       </Stack.Navigator>
     </NavigationContainer> 
   );
