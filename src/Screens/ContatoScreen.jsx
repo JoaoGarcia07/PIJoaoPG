1624f36b3e8d2aba8656a1325c108f48e723c657
JoaoGarcia07 <joao.garcia2@alunos.sc.senac.br>
2022-11-03 16:59:37 -0300

    pag Contato

---
+++ src/Screens/ContatoScreen.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { Text, View } from 'react-native'
+import {
+  RadioButton,
+  Searchbar,
+  TextInput,
+  ToggleButton,
+} from 'react-native-paper'
+import { styles } from '../lib/styles'
+
+export const ContatoScreen = () => {
+  const [text, setText] = useState('')
+  const [value, setValue] = useState('left')
+  const [checked, setChecked] = useState('first')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const onChangeSearch = (query) => setSearchQuery(query)
+
+  return (
+    <View style={styles.container}>
+      <Text>Eu sou a Página Contato</Text>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+
+      <RadioButton
+        value="first"
+        status={checked === 'first' ? 'checked' : 'unchecked'}
+        onPress={() => setChecked('first')}
+      />
+      <RadioButton
+        value="second"
+        status={checked === 'second' ? 'checked' : 'unchecked'}
+        onPress={() => setChecked('second')}
+      />
+
+      <TextInput
+        label="Email"
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+
+      <ToggleButton.Row
+        onValueChange={(value) => setValue(value)}
+        value={value}
+      >
+        <ToggleButton icon="format-align-left" value="left" />
+        <ToggleButton icon="format-align-right" value="right" />
+      </ToggleButton.Row>
+    </View>
+  )
+}
--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -4,8 +4,9 @@ import { styles } from '../lib/styles'
 export const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>OI eu seu a Home Screen</Text>
+      <Text>Oi eu sou Home Screen</Text>
       <Button title="Ir para Sobre" onPress={() => navigation.navigate('Sobre')} />
+      <Button title="Ir para Contato" onPress={() => navigation.navigate('Contato')} />
     </View>
   )
 }
